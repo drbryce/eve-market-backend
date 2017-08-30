6e5c5cd2b061ca1bcb1344fa8f386609e44296f1
drbryce <bryce.e.harrison@gmail.com>
2017-08-30 19:30:53 -0400

    getting started

--- app.js
+++ app.js
@@ -4,10 +4,17 @@ var path = require('path')
 var logger = require('morgan')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
+var mongoose = require('mongoose')
+var options = require('options')
 var index = require('./routes/index')
 var users = require('./routes/users')
+var staticdata = require('./routes/staticdata')
 
 var app = express()
+
+mongoose.connect(options.mongooseConnectionString)
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error: '))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
@@ -23,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', index)
 app.use('/users', users)
+app.use('/staticdata', staticdata)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- options.js
+++ options.js
@@ -2,3 +2,9 @@
 exports.clientId = '3779a95b42a742db99a92fce95fa1336 '
 exports.secretKey = 'r8C1KHn9BanoRc4NQjPyQkZ2CqDxlDcb9WwUgrzT'
 exports.callBackURL = 'http://localhost:3000/eve_sso_callback'
+
+// sqlite eve static data DB
+exports.sqliteDB = 'sqlite-latest.sqlite'
+
+// mongodb connection url
+exports.mongoConnectString = 'mongodb://localhost/eve-market-app'
