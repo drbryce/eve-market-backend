dc600fa1ae0cf13be7184d46f259dfa338b146e7
drbryce <bryce.e.harrison@gmail.com>
2017-08-30 19:09:19 -0400

    Update options.js

--- options.js
+++ options.js
@@ -1,4 +1,4 @@
 // Eve SSO
-exports.clientId = '3779a95b42a742db99a92fce95fa1336 '
-exports.secretKey = 'r8C1KHn9BanoRc4NQjPyQkZ2CqDxlDcb9WwUgrzT'
-exports.callBackURL = 'http://localhost:3000/eve_sso_callback'
+exports.clientId = ''
+exports.secretKey = ''
+exports.callBackURL = ''
